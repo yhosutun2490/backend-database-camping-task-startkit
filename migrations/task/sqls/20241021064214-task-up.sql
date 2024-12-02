491af2e74b11ea3672abf418e2ef150d610a6923
yhosutun2491 <yhosutun2491@gmail.com>
2024-12-02 23:14:14 +0800

    <feat> finish task 2

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -14,7 +14,7 @@
 --     4. 用戶名稱為`好野人`，Email 為`richman@hexschooltest.io`，Role為`USER`
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
 --     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
-INSERT INTO "USER"(name, email, role) VALUES
+insert into "USER"(name, email, role) values
 ('李燕容','lee2000@hexschooltest.io' , 'USER'),
 ('王小明', 'wXlTq@hexschooltest.io', 'USER'),
 ('肌肉棒子', 'muscle@hexschooltest.io', 'USER'),
@@ -26,16 +26,16 @@ INSERT INTO "USER"(name, email, role) VALUES
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 update "USER" set role = 'COACH'
 where  email in ('lee2000@hexschooltest.io','muscle@hexschooltest.io','starplatinum@hexschooltest.io')
-and role = 'USER'
+and role = 'USER';
 
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
 delete from "USER"
-where email = 'opacity0@hexschooltest.io'
+where email = 'opacity0@hexschooltest.io';
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
 select count(*) as "目前用戶數" from "USER" 
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
-select * from "USER" limit 3
+select * from "USER" limit 3;
 
 --  ████████  █████   █    ████  
 --    █ █   ██    █  █         █ 
@@ -48,11 +48,35 @@ select * from "USER" limit 3
     -- 1. 名稱為 `7 堂組合包方案`，價格為`1,400` 元，堂數為`7`
     -- 2. 名稱為`14 堂組合包方案`，價格為`2,520` 元，堂數為`14`
     -- 3. 名稱為 `21 堂組合包方案`，價格為`4,800` 元，堂數為`21`
+insert into "CREDIT_PACKAGE"(name, price, credit_amount) values 
+('7 堂組合包方案',1400,7),
+('14 堂組合包方案',2520,14),
+('21 堂組合包方案',4800,21);
 
 -- 2-2. 新增：在 `CREDIT_PURCHASE` 資料表，新增三筆資料：（請使用 name 欄位做子查詢）
     -- 1. `王小明` 購買 `14 堂組合包方案`
     -- 2. `王小明` 購買 `21 堂組合包方案`
     -- 3. `好野人` 購買 `14 堂組合包方案`
+INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, price_paid, purchased_credits)
+VALUES
+(
+    (SELECT id FROM "USER" WHERE name = '王小明'),
+    (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+    (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+    1
+),
+(
+    (SELECT id FROM "USER" WHERE name = '王小明'),
+    (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
+    (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
+    1
+),
+(
+    (SELECT id FROM "USER" WHERE name = '好野人'),
+    (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+    (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+    1
+);
 
 
 -- ████████  █████   █    ████   
